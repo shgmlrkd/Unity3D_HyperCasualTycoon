811cab2904309f2e0f5c2f66b634c3943fa40a2a
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-09 04:55:31 +0900

    Feat : Meney, Upgrade popup 완료
    Feat : Meney, Upgrade popup 완료

---
+++ Assets/03.Scripts/UI/UpGradePopup/ContentsScrollView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ContentsScrollView : MonoBehaviour
+{
+    [SerializeField] private Transform contentTransform; // Scroll View -> Viewport -> Content 오브젝트 할당
+    [SerializeField] private GameObject itemPrefab;       // 스크롤 뷰에 넣을 아이템 프리팹 할당
+
+    private void Awake()
+    {
+        CreateScrollItems(3);
+    }
+
+    private void CreateScrollItems(int count)
+    {
+        // 기존에 생성되어 있던 아이템이 있다면 삭제 (초기화 필요할 때 사용)
+        foreach (Transform child in contentTransform)
+        {
+            Destroy(child.gameObject);
+        }
+        // 반복문을 돌며 프리팹 동적 생성
+        for (int i = 0; i < count; i++)
+        {
+            // 1. 프리팹을 생성하면서 부모(Content)를 지정합니다.
+            GameObject newItem = Instantiate(itemPrefab, contentTransform);
+
+            // 2. 프리팹 내부의 컴포넌트를 가져와 데이터를 변경합니다.
+            // (예: 아이템 스크립트가 있다면 데이터를 세팅)
+            Content itemScript = newItem.GetComponent<Content>();
+            if (itemScript != null)
+            {
+                //itemScript.loadData(
+                                                
+                //    )
+            }
+        }
+    }
+}
---
+++ Assets/03.Scripts/UI/UpGradePopup/UpGradePopup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpGradePopup : MonoBehaviour
+{
+    //close 버튼
+    [SerializeField] private Button closePopupBtn;
+    //popup
+    [SerializeField] GameObject popup;
+    private void Awake()
+    {
+        closePopupBtn.onClick.AddListener(() => onClickClosePopup());
+    }
+    //onClickClosePopup
+    //20260609
+    //js.shin
+    //Close Popup
+    private void onClickClosePopup()
+    {
+        popup.SetActive(false);
+        
+    }
+}
