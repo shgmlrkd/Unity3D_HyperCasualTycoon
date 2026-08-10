b5f5f282f87cc97e9c25047a395009fea3aeb026
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-10 10:36:34 +0900

    Feat : UI - InsertCircle, InsertBuild 추가
    InsertCircle
    InsertBuild 추가

---
+++ Assets/03.Scripts/UI/InsertBuild/InsertBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class InsertBuild : MonoBehaviour
+{
+    //빌딩 건설시 지불금액
+    [SerializeField] private TextMeshProUGUI money;
+
+    private void SetBuildMoney(int pay)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(pay);
+        money.SetText(sb.ToString());
+    }
+}
---
+++ Assets/03.Scripts/UI/InsertCircle/InsertCircle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class InsertCircle : MonoBehaviour
+{
+    
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+    }
+}
