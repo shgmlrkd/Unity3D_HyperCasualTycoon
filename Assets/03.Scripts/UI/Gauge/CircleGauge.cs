c7564551801dae201544a10f1c6234b9043189f6
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-11 15:21:43 +0900

    Refector : UI - CircleGauge, InsertCircle, UpGradePopup 수정
    InsertCircle안에서 이벤트(팝업, 쓰레기, 음식) 수정
    InsertCircle
    - type : 1 (팝업)
    - type : 2 (음식), Food Img(음식 사진)
    - type : 3 (쓰레기)

--- Assets/03.Scripts/UI/Gauge/CircleGauge.cs
+++ Assets/03.Scripts/UI/Gauge/CircleGauge.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+
 public class CircleGauge : MonoBehaviour
 {
-    [SerializeField] private GameObject circleGauge;
     [SerializeField] private Image itemSlider; //게이지 이미지
     [SerializeField] private Image foodImg; // 푸드이지미 -  기본은 이미지 없음
     [SerializeField] private float itemCoolDownTime = 5.0f; //쿨타임
-    float updateTime = 0.0f; //타임
+    private float updateTime = 0.0f; //타임
+    //private bool complit = false; //게이지 완료
+    public bool Complit {  get; set; }
 
-    
+
+
+    private void Awake()
+    {
+        //비활성화
+        gameObject.SetActive(false);
+        Complit = false;
+    }
 
     private void Update()
     {
+        if (Complit) return;
         //쿨타임 초과시 초기화
         if (updateTime > itemCoolDownTime)
         {
-            updateTime = 0.0f;
-            itemSlider.fillAmount = 0.0f;
+            //시간, fillAmount 초기화
+            SetResetData();
         }
         else
         {
@@ -27,13 +37,22 @@ public class CircleGauge : MonoBehaviour
 
             //fillAmount 증가
             itemSlider.fillAmount = (Mathf.Lerp(0, 100, updateTime / itemCoolDownTime) / 100);
+            //게이지 완료
+            if (1 <= itemSlider.fillAmount)
+            {
+                Complit = true;
+            }
         }
     }
-    //public void SetActiveGauge()
-    //{
-    //    Debug.Log("TEst");
-    //    //circleGauge.SetActive(true); 
-    //}
+    //20260811
+    //JS.S
+    //SetActiveGauge : 활성화
+    //para 
+    //active : true, false
+    public void SetActiveGauge(bool active)
+    {
+        gameObject.SetActive(active);
+    }
     //20260809
     //JS.S
     //SetFoodImg : foot img setting
@@ -41,13 +60,32 @@ public class CircleGauge : MonoBehaviour
     //image : food Img
     public void SetFoodImg(Sprite image)
     {
-        if(image==null)return;
         foodImg.sprite = image;
     }
-    public void Test()
+    //20260811
+    //JS.S
+    //SetResetData : 시간, fillAmount 초기화
+    public void SetResetData()
     {
-        Debug.Log("TEst");
-       // circleGauge.SetActive(true);
+        updateTime = 0.0f;
+        itemSlider.fillAmount = 0.0f;
     }
+
+    ////20260811
+    ////JS.S
+    ////GetComplit : Get Complit
+    //public bool GetComplit()
+    //{
+    //    return complit;
+    //}
+    ////20260811
+    ////JS.S
+    ////SetComplit : Set Complit
+    //public void SetComplit(bool complit)
+    //{
+    //    this.complit = complit;
+    //}
+
+
 
 }
