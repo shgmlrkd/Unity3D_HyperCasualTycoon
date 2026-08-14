867969d6230dfead4952612e36ffe7c1acf6e591
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-14 16:55:15 +0900

    Refector : UI - money 지불 모션 수정, money prefab 수정
    - money 지불 모션 수정 완료
    - money prefab 수정 완료

--- Assets/03.Scripts/UI/MoneyShooter/MoneyShooter.cs
+++ Assets/03.Scripts/UI/MoneyShooter/MoneyShooter.cs
@@ -1,60 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
 public class MoneyShooter : MonoBehaviour
 {
-    [SerializeField] private GameObject throwPrefab; // 던질 프리팹
-    [SerializeField] private Transform startPoint;    // 던지는 시작 위치
-    //[SerializeField] private Transform endPoint;
-    
+    [SerializeField] private GameObject moneyPrefab; // 머니 프리팹
 
-    [SerializeField] float speed = 0.5f;
-
-    private Transform endPoint;
-
-    //private Transform endPoint2;
-    private bool isStart = false;
-    public bool GetIsStart()
+    //202600814
+    //js.shin
+    //ShootMoneny : moneyPrefab 생성
+    //para
+    //endPoint : 도착 위치
+    public void ShootMoneny(Transform endPoint)
     {
-        return isStart;
-    }
-    public void SetIsStart(bool isStart)
-    {
-        this.isStart = isStart; 
-    }
-
-
-    //private void Awake()
-    //{
-    //    endPoint2 = 
-    //}
-
-    public void SetEndPoint(Transform endPoin)
-    {
-        this.endPoint = endPoint;
-    }
-
-    void Update()
-    {
-        if (!isStart) return;
-        GameObject instance = Instantiate(throwPrefab, startPoint.position, Quaternion.identity);
-        StartCoroutine(MoveToDestination(instance));
-           
-    }
-
-    private IEnumerator MoveToDestination(GameObject obj)
-    {
-        isStart = false;
-        while (obj != null && Vector3.Distance(obj.transform.position, endPoint.position) > 0.01f)
-        {
-            // 도착지까지 매 프레임 이동
-            obj.transform.position = Vector3.MoveTowards(
-                obj.transform.position,
-                endPoint.position,
-                speed * Time.deltaTime
-            );
-            yield return 0.5f;
-        }
-        Destroy(obj);
-        isStart = true;
+        //moneyPrefab 생성
+        GameObject instance = Instantiate(moneyPrefab, gameObject.transform.position, Quaternion.identity);
+        //moneyPrefab 
+        MoneyObj moneyObj = instance.GetComponent<MoneyObj>();
+        //moneyPrefab EndPoint setting
+        moneyObj.SetEndPoint(endPoint);  
     }
 }
