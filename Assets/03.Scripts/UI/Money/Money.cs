e09a1c9e80aa8712f06b9dfd3f5a2ef2fe2fdccc
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-14 18:37:14 +0900

    Feat : UI - money prefab추가
    - money prefab추가

---
+++ Assets/03.Scripts/UI/Money/Money.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Money : MonoBehaviour
+{
+    [SerializeField] private int money = 0;
+
+    public void SetMoney(int money)
+    {
+        this.money = money;
+    }
+
+    public void DestroyMoney()
+    {
+        Destroy(gameObject);    
+    }
+}
