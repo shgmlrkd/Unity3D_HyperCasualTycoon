36bcc8460017069e689c34c524e374bf848338be
shgmlrkd <jack342@naver.com>
2026-08-15 11:30:43 +0900

    refactor: Update UnlockPointManager for Save and Load
    - UnlockPointManager에서 세이브/로드 기능을 사용할 수 있도록 수정
    - 기존 데이터 처리 로직을 세이브/로드 시스템에 맞게 변경

--- Assets/03.Scripts/Building/Restaurant/Test/UnlockPointManager.cs
+++ Assets/03.Scripts/Building/Restaurant/Test/UnlockPointManager.cs
@@ -15,13 +15,6 @@ public class UnlockPointManager : LocalSingleton<UnlockPointManager>
         base.Awake();
 
         unlockPoints = GetComponentsInChildren<UnlockPoint>(true);
-        //Initialize();
-        //LoadUnlockPoint(currentUnlockPointIndex);
-    }
-
-    private void Start()
-    {
-        //unlockPoints = GetComponentsInChildren<UnlockPoint>(true);
 
         // SaveManager에 저장되어 넘어온 해금 인덱스가 있다면 적용
         int targetIndex = 0;
@@ -33,20 +26,9 @@ public class UnlockPointManager : LocalSingleton<UnlockPointManager>
             targetIndex = SaveManager.Instance.CurrentData.CurrentUnlockIndex;
         }
 
-        //Initialize();
         LoadUnlockPoint(targetIndex);
 
         Debug.Log($"[UnlockPointManager] 해금 단계 {targetIndex} 번으로 세팅 완료!");
-    }
-
-    private void Initialize()
-    {
-        for (int i = 0; i < unlockPoints.Length; i++)
-        {
-            unlockPoints[i].gameObject.SetActive(i == currentUnlockPointIndex);
-        }
-
-        SubscribeCurrentUnlockPoint();
     }
 
     private void SubscribeCurrentUnlockPoint()
@@ -77,20 +59,15 @@ public class UnlockPointManager : LocalSingleton<UnlockPointManager>
 
     public void LoadUnlockPoint(int currentUnlockIndex)
     {
-        //UnsubscribeCurrentUnlockPoint();
-
         if(unlockPoints == null || unlockPoints.Length == 0)
         {
             Debug.LogWarning("[UnlockPointManager] UnlockPoint가 없어요");
             return;
         }
 
-       
         Debug.Log($"현재 언락 포인트 인덱스 값은! currentUnlockPointIndex : " + currentUnlockPointIndex);
         Debug.Log($"현재 언락 인덱스는! currentUnlockIndex : " + currentUnlockIndex);
         
-        //currentUnlockPointIndex = currentUnlockIndex;
-
         currentUnlockIndex = Mathf.Clamp(currentUnlockIndex, 0, unlockPoints.Length -1);
 
         UnsubscribeCurrentUnlockPoint();
