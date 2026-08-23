2f82ba7eae52aa190bbc0531735a9563c3b9dc5f
shgmlrkd <jack342@naver.com>
2026-08-23 17:58:02 +0900

    refactor : refactor NPC AnimationController Scripts
    - 손님, 직원 NPC 애니메이션 스크립트 추가 및 수정

--- Assets/03.Scripts/NPC/Employee/EmployeeAnimationController.cs
+++ Assets/03.Scripts/NPC/Employee/EmployeeAnimationController.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class EmployeeAnimationController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private const string ISCARRY = "IsCarry";
+
+    [SerializeField]
+    private Animator animator;
+
+    private int isStoppedHash = Animator.StringToHash("IsStopped");
+
+    private void Awake()
     {
-        
+        if(animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 걷는 애니메이션
+    public void SetMove(bool isStopped)
     {
-        
+        animator.SetBool(isStoppedHash, isStopped);
+    }
+
+    // 서빙할 음식 들고있는 애니메이션
+    public void SetPlayCarry(bool hasCarriedItem)
+    {
+        animator.SetBool(ISCARRY, hasCarriedItem);
     }
 }
