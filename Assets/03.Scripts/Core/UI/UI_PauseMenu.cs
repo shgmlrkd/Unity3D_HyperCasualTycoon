33b31d142fcfe01b32a1a7f84827dc0b924fb615
“ShinJinSeop2536” <70467644+ShinJinSeop2536@users.noreply.github.com>
2026-08-27 14:05:49 +0900

    Refector : UI - PauseMenu 수정
    PauseMenu
    - 디자인 수정
    - 두번쨰 나오는 오류 수정

--- Assets/03.Scripts/Core/UI/UI_PauseMenu.cs
+++ Assets/03.Scripts/Core/UI/UI_PauseMenu.cs
@@ -1,10 +1,14 @@
+﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.UI;
 
 public class UI_PauseMenu : MonoBehaviour
 {
     [Header("Panels")]
     [SerializeField] private GameObject pausePanel;
     [SerializeField] private GameObject optionPanel;
+    [SerializeField] private GameObject contentPanel;
 
     [Header("Confirm Modal Popup")]
     [SerializeField] private UI_ConfirmModal confirmModal;
@@ -12,12 +16,24 @@ public class UI_PauseMenu : MonoBehaviour
     [Header("Scene Settings")]
     [SerializeField] private string titleSceneName = "TitleScene";
 
+    [SerializeField] private Button pauseBtn;
+    [SerializeField] private Button closeBtn;
+
+    //private bool openState = false;
+
     private void Start()
     {
         if (EventManager.Instance != null)
         {
             EventManager.Instance.Subscribe(EventType.OnGameStateChanged, OnGameStateChanged);
         }
+        //GameManager.Instance.PauseGame();
+        GameManager.Instance.ResumeGame();
+        pauseBtn.onClick.AddListener(() => OnOpenClosePause());//upgrade버튼
+        closeBtn.onClick.AddListener(() => OnOpenClosePause());//upgrade버튼
+
+
+
     }
 
     private void OnDestroy()
@@ -32,22 +48,30 @@ public class UI_PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (optionPanel != null && optionPanel.activeSelf)
-            {
-                OnClickCloseOption();
-                return;
-            }
+            OnOpenClosePause();
+        }
 
-            if (GameManager.Instance != null)
+    }
+    
+    private void OnOpenClosePause()
+    {
+        if (optionPanel != null && optionPanel.activeSelf)
+        {
+            OnClickCloseOption();
+            //return;
+        }
+
+        if (GameManager.Instance != null)
+        {
+            if (GameManager.Instance.CurrentState == GameState.Play)
             {
-                if (GameManager.Instance.CurrentState == GameState.Play)
-                {
-                    GameManager.Instance.PauseGame();
-                }
-                else if (GameManager.Instance.CurrentState == GameState.Pause)
-                {
-                    GameManager.Instance.ResumeGame();
-                }
+                
+                GameManager.Instance.PauseGame();
+
+            }
+            else if (GameManager.Instance.CurrentState == GameState.Pause)
+            {
+                GameManager.Instance.ResumeGame();
             }
         }
     }
@@ -57,7 +81,52 @@ public class UI_PauseMenu : MonoBehaviour
         if (GameManager.Instance == null || pausePanel == null) return;
 
         bool isPaused = (GameManager.Instance.CurrentState == GameState.Pause);
+
         pausePanel.SetActive(isPaused);
+
+        
+        //if (isPaused)
+        //{
+        //    //pausePanel.SetActive(isPaused);
+        //    //if (openState) return;
+
+        //    pausePanel.transform.DOScale(1.0f, 1.0f);
+
+        //    // DOScale 의 첫 번째 파라미터는 목표 Scale 값, 두 번째는 시간입니다.
+        //    //seq.Append(pausePanel.transform.DOScale(1.1f, 0.2f));
+        //    //seq.Append(pausePanel.transform.DOScale(1f, 0.1f));
+        //    //seq.Play().OnComplete(() =>
+        //    //{
+        //    //    //pausePanel.SetActive(isPaused);
+                
+        //    //    //openState = true;
+        //    //    //OpenState = true;
+        //    //});
+            
+        //}
+        //else
+        //{
+        //    var seq = DOTween.Sequence();
+        //    //if(!pausePanel.activeSelf)return;
+        //    //if (!openState) return;
+
+        //    //pausePanel.transform.localScale = Vector3.one * 0.2f;
+
+        //    seq.Append(pausePanel.transform.DOScale(1.1f, 0.1f));
+        //    seq.Append(pausePanel.transform.DOScale(0.2f, 0.2f));
+
+        //    // OnComplete 는 seq 에 설정한 애니메이션의 플레이가 완료되면
+        //    // { } 안에 있는 코드가 수행된다는 의미입니다.
+        //    // 여기서는 닫기 애니메이션이 완료된 후 객첼르 비활성화 합니다.
+        //    seq.Play().OnComplete(() =>
+        //    {
+        //        pausePanel.SetActive(isPaused);
+        //        pausePanel.transform.DOScale(1.0f, 1.0f);
+        //        //openState = false;
+        //    });
+        //    //pausePanel.SetActive(isPaused);
+        //}
+
     }
 
     public void OnClickResume()
@@ -127,6 +196,7 @@ public class UI_PauseMenu : MonoBehaviour
         if (optionPanel != null)
         {
             optionPanel.SetActive(true);
+            contentPanel.SetActive(false);
         }
     }
 
@@ -135,6 +205,7 @@ public class UI_PauseMenu : MonoBehaviour
         if (optionPanel != null)
         {
             optionPanel.SetActive(false);
+            contentPanel.SetActive(true);
         }
     }
 }
